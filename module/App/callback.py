2345d584fb0b1509f9927bdf16d18fa6db47df0e
yeryeong <88486391+yeryeong713@users.noreply.github.com>
2023-11-16 23:54:18 +0900

    Update callback.py
    파일 수정하였습니다.

--- module/App/callback.py
+++ module/App/callback.py
@@ -1,23 +1,56 @@
-class CallbackManager:
-  # 위치 및 위도, 경도 데이터
-  def location(self):
-        data = db.child("information").get().val()
-        if data:
-            entry = data[0]  # Assuming there is only one entry in the "information" array
-            return {
-                "Requested Location": entry.get("Requested Location"),
-                "Latitude (DD)": entry.get("Latitude (DD)"),
-                "Longitude (DD)": entry.get("Longitude (DD)")
+from dash.dependencies import Output, Input, State
+from layout import app
+from firebase import FirebaseManager
+
+firebase_manager = FirebaseManager()
+db = firebase_manager.db
+
+@app.callback(
+    Output('location-output', 'children'),
+    Input('dummy-input', 'value')
+)
+def update_location(dummy_value):
+    data = db.child("information").get().val()
+
+    if data:
+        entry = data[0]
+        location_info = {
+            "Requested Location": entry.get("Requested Location"),
+            "Latitude (DD)": entry.get("Latitude (DD)"),
+            "Longitude (DD)": entry.get("Longitude (DD)")
+        }
+        return f'<Requested Location>: {location_info["Requested Location"]}, <Latitude>: {location_info["Latitude (DD)"]}, <Longitude>: {location_info["Longitude (DD)"]}'
+    else:
+        return 'No data available'
+
+@app.callback(
+    Output('solar-power-graph', 'figure'),
+    [Input('ok-button', 'n_clicks')],
+    [State('month-dropdown', 'value'),
+     State('day-dropdown', 'value')]
+)
+def update_graph(n_clicks, selected_month, selected_day):
+    data = db.child("pvwatts").get().val()
+
+    if data and n_clicks > 0:
+        dc_output_data = []
+        for entry in data:
+            if entry.get("Month") == selected_month and entry.get("Day") == selected_day:
+                dc_output_data.append(entry.get("DC Array Output (W)", 0))
+
+        figure = {
+            'data': [
+                {'x': list(range(len(dc_output_data))), 'y': dc_output_data, 'type': 'line', 'name': 'Solar Power Generation'},
+            ],
+            'layout': {
+                'title': f'Solar Power Generation for {selected_month}/{selected_day}',
+                'xaxis': {'title': 'Time (hours)'},
+                'yaxis': {'title': 'Solar Power (W)'},
             }
-          
-  # 태양광 전력량 데이터 (1시간 단위)
-  def get_dc_output_data(self, target_month, target_day):
-      data = db.child("pvwatts").get().val()
-      if data:
-          dc_output_data = []
-          for entry in data:
-              if entry.get("Month") == target_month and entry.get("Day") == target_day:
-                  dc_output_data.append(entry.get("DC Array Output (W)", 0))
-          return dc_output_data
-      else:
-          return None
+        }
+        return figure
+    else:
+        return {'data': []}
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
